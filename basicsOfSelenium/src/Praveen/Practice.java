a36b4149626964cff2163290a6f746d748436c67
win10 <win10@DESKTOP-P6MK8TL>
2023-01-24 12:45:17 +0530

    added author name

--- basicsOfSelenium/src/Praveen/Practice.java
+++ basicsOfSelenium/src/Praveen/Practice.java
@@ -1,7 +1,11 @@
 package Praveen;
-
+/***
+ * 
+ * @author win10
+ *
+ */
 public class Practice {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello priya");
